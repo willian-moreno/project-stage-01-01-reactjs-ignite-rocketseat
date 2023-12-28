4b635c8a101153831f3b5ee54f2c62f6b7fe8d95
willianam <willianam@uninove.br>
2023-12-28 18:50:30 -0300

    style: Logo e estilos header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,12 @@ import style from './style.module.css'
 export function Header() {
   return (
     <header className={style.header}>
-      <strong>Ignite Feed</strong>
+      <img
+        className={style.header__logo}
+        src="/src/assets/ignite-simbol.svg"
+        alt="Logo ignite"
+      />
+      <strong className={style.header__title}>Ignite Feed</strong>
     </header>
   )
 }
